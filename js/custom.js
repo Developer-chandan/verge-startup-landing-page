4b8617ff0d31e6f3f0d4c773c6a8477ad9e6845e
Chandan sen <chandan2441139@gmail.com>
2024-01-19 23:44:56 +0600

    responsive with animation

--- js/custom.js
+++ js/custom.js
@@ -30,3 +30,41 @@ function hideMenu() {
     // Hide the menu by setting its display property to 'none'
     mobileMenu.classList.remove('show');
   }
+
+
+
+  // logo circle 
+
+  document.addEventListener("DOMContentLoaded", function() {
+    window.addEventListener("scroll", function() {
+        var scrollY = window.scrollY;
+        var rotation = "rotate(" + scrollY + "deg)";
+        document.getElementById("verge_logo").style.transform = rotation;
+    });
+});
+
+
+
+// observer image 
+
+document.addEventListener("DOMContentLoaded", function() {
+  const images = document.querySelectorAll(".visible-image");
+
+  const observer = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              entry.target.style.opacity = "1";
+              entry.target.style.transform = "translateY(0)";
+
+          } else {
+              entry.target.style.opacity = "0";
+              // entry.target.style.transform = "translateY(200px)";
+
+          }
+      });
+  }, { threshold: 0.5 }); // Adjust the threshold as needed
+
+  images.forEach(image => {
+      observer.observe(image);
+  });
+});
