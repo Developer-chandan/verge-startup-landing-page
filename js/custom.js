1ef53e307dbffac81526f68810a786b22e072b21
Chandan sen <chandan2441139@gmail.com>
2024-01-19 22:24:43 +0600

    update responsive and functions

--- js/custom.js
+++ js/custom.js
@@ -5,12 +5,28 @@ const mobileMenuCross = document.querySelector('.navbar_toggle_cross');
 
 
 mobileMenuButton.addEventListener('click', () => {
-    mobileMenu.classList.add('show');
+    mobileMenu.classList.toggle('show');
+    if (mobileMenu.classList.contains('hide')) {
+        mobileMenu.classList.remove('hide');
+        
+      } 
+    
 
 })
 
 mobileMenuCross.addEventListener('click', () => {
     mobileMenu.classList.add('hide');
 
+    if (mobileMenu.classList.contains('hide')) {
+        mobileMenu.classList.remove('show');
+      } 
+
 })
 
+function hideMenu() {
+    // Get the <ul> element
+    var menuList = document.querySelector('ul li a');
+
+    // Hide the menu by setting its display property to 'none'
+    mobileMenu.classList.remove('show');
+  }
